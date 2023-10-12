ef8330de63aecb6bac91a016fd8fde9a0da99f27
SupriyaKodekanti <144899172+SupriyaKodekanti@users.noreply.github.com>
2023-10-12 11:19:44 +0530

    Update Abc.java

--- src/main/java/abcd.java
+++ src/main/java/abcd.java
@@ -1,9 +1,8 @@
 
-public class abcd {
-	public String b;
+public class Abc {
 
 	public static void main(String args[]) {
-		public String a;
+		public String a="Welcome";
 		System.out.println(a);
 		System.out.println("hi "");
 	}
